3cb8351f5375b046fdcf3d845f29b896f973e84e
Chunchunmaru <thomasgabinete@gmail.com>
2024-10-15 16:15:28 +0800

    Refactor: Modified some php

---
+++ .history/app/Views/layout/main_layout_20241014211410.php
@@ -0,0 +1,6 @@
+<?= $this->include('components/top_layer') ?>
+<?= $this->renderSection('contents') ?>
+<?= $this->include('components/header_nav') ?>
+
+
+<?= $this->include('components/bottom_layer') ?>
